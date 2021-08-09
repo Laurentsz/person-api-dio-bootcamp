3a83ff6f844c5f9e18c7dca1c53722d36c0c71aa
Rai Laurentino <rai.laurentino@digisystem.com.br>
2021-08-09 13:13:10 -0300

    feat(development) Create Person Service
    Added POST Request + Service for create new person.

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -3,22 +3,23 @@ package one.digitalinnovation.personapi.controller;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
+import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/person")
 public class PersonController {
 
+
     @Autowired
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @PostMapping
-    public MessageResponseDTO newPerson(@RequestBody Person person){
-        Person savedPerson =  personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created Person, ID #" + savedPerson.getId())
-                .build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        return personService.newPerson(person);
     }
+
 }
