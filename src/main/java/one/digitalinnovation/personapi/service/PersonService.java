be17b1b789898a5d212f1fbdfd314521a8976277
Rai Laurentino <rai.laurentino@digisystem.com.br>
2021-08-09 21:12:30 -0300

    Feat(development) - Adding DTO's

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,12 +1,11 @@
 package one.digitalinnovation.personapi.service;
 
-import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PersonService {
